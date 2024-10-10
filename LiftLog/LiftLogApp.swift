2465ca39e1acf816eaa76069fe2cc79e2197941b
KhalilKobeissi <kobeissikhalil@gmail.com>
2024-10-10 20:48:57 +0200

    Initial Commit

---
+++ LiftLog/LiftLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiftLogApp.swift
+//  LiftLog
+//
+//  Created by Khalil Kobeissi on 10.10.24.
+//
+
+import SwiftUI
+
+@main
+struct LiftLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
